f92f6fec7e51a9e58a59eb24357a5dc2a3df43d4
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-10-06 15:06:46 +0900

    모델은 띄웠는데 CRenderer 굳이 쓸 필요가 있나

--- Engine/Private/Level.cpp
+++ Engine/Private/Level.cpp
@@ -1,13 +1,16 @@
 #include "..\Public\Level.h"
 
 #include "GameInstance.h"
+#include "RenderInstance.h"
 
 CLevel::CLevel(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 	: m_pDevice{ pDevice }
 	, m_pContext{ pContext }
 	, m_pGameInstance{ CGameInstance::Get_Instance() }
+	, m_pRenderInstance{ CRenderInstance::Get_Instance() }
 {
 	Safe_AddRef(m_pGameInstance);
+	Safe_AddRef(m_pRenderInstance);
 	Safe_AddRef(m_pDevice);
 	Safe_AddRef(m_pContext);
 }
@@ -29,7 +32,7 @@ HRESULT CLevel::Render()
 
 HRESULT CLevel::Clear_Resources()
 {
-	return m_pGameInstance->Clear_LevelResources(m_iLevelIndex);	
+	return m_pGameInstance->Clear_LevelResources(m_iLevelIndex);
 }
 
 void CLevel::Free()
@@ -37,6 +40,7 @@ void CLevel::Free()
 	__super::Free();
 
 	Safe_Release(m_pGameInstance);
+	Safe_Release(m_pRenderInstance);
 	Safe_Release(m_pDevice);
 	Safe_Release(m_pContext);
 }
