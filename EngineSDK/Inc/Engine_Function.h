b86b4803333b0254febfd794fb2c59b8ba987eb2
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-10-06 20:13:44 +0900

    이제 진짜 스마트포인터 만들어보자

--- EngineSDK/Inc/Engine_Function.h
+++ EngineSDK/Inc/Engine_Function.h
@@ -50,7 +50,6 @@ namespace Engine
 		return iRefCnt;
 	}
 
-	
 	// 1. 벡터의 길이를 구하는 함수
 	inline _float GetVectorLength(const _vector& v)
 	{
@@ -73,10 +72,77 @@ namespace Engine
 		return floatMat;
 	}
 
-	//4. Float4x4를 Matrix를
+	//4. Float4x4를 Matrix를 바꿔줌
 	inline _matrix Float4x4ToMatrix(const _float4x4& floatMat)
 	{
 		return XMLoadFloat4x4(&floatMat);
 	}
-}
 
+	// 5. _float4 더하기
+	inline _float4 AddFloat4(const _float4& f1, const _float4& f2)
+	{
+		_float4 result;
+		result.x = f1.x + f2.x;
+		result.y = f1.y + f2.y;
+		result.z = f1.z + f2.z;
+		result.w = f1.w + f2.w;
+		return result;
+	}
+
+	// 6. _float4 빼기
+	inline _float4 SubtractFloat4(const _float4& f1, const _float4& f2)
+	{
+		_float4 result;
+		result.x = f1.x - f2.x;
+		result.y = f1.y - f2.y;
+		result.z = f1.z - f2.z;
+		result.w = f1.w - f2.w;
+		return result;
+	}
+
+	// 7. _float4 곱하기
+	inline _float4 MultiplyFloat4(const _float4& f1, const _float4& f2)
+	{
+		_float4 result;
+		result.x = f1.x * f2.x;
+		result.y = f1.y * f2.y;
+		result.z = f1.z * f2.z;
+		result.w = f1.w * f2.w;
+		return result;
+	}
+
+	// 8. _float4 나누기
+	inline _float4 DivideFloat4(const _float4& f1, const _float4& f2)
+	{
+		_float4 result;
+		result.x = f1.x / f2.x;
+		result.y = f1.y / f2.y;
+		result.z = f1.z / f2.z;
+		result.w = f1.w / f2.w;
+		return result;
+	}
+
+	//9. _float4x4와 _matrix를 곱하는 함수
+	inline _matrix Multiply_Float4x4_Matrix(const _float4x4& f4x4, const _matrix& mat)
+	{
+		// _float4x4를 XMMATRIX로 변환
+		XMMATRIX xmMat1 = XMLoadFloat4x4(&f4x4);
+
+		// 두 XMMATRIX를 곱하고 결과 반환
+		XMMATRIX result = XMMatrixMultiply(xmMat1, mat);
+
+		return result;
+	}
+
+	//10. _float4x4와 _matrix를 곱하는 함수
+	inline _matrix Multiply_Matrix_Float4x4(const _matrix& mat, const _float4x4& f4x4)
+	{
+		// _float4x4를 XMMATRIX로 변환
+		XMMATRIX xmMat1 = XMLoadFloat4x4(&f4x4);
+
+		// 두 XMMATRIX를 곱하고 결과 반환
+		XMMATRIX result = XMMatrixMultiply(mat, xmMat1);
+
+		return result;
+	}
+}
