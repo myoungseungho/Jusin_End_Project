729e92f231d95a372d6a1975fe926cad9f900b7b
kimtaewan25 <kimtaewan25@gmail.com>
2024-10-08 16:36:16 +0900

    임시

--- Engine/Private/Camera.cpp
+++ Engine/Private/Camera.cpp
@@ -19,10 +19,10 @@ HRESULT CCamera::Initialize_Prototype()
 
 	_uint		iNumViewports = { 1 };
 
-	m_pContext->RSGetViewports(&iNumViewports, &ViewPortDesc);
+	//m_pContext->RSGetViewports(&iNumViewports, &ViewPortDesc);
 
-	m_fViewportWidth = ViewPortDesc.Width;
-	m_fViewportHeight = ViewPortDesc.Height;
+	m_fViewportWidth = 1280;
+	m_fViewportHeight = 720;
 
 	return S_OK;
 }
