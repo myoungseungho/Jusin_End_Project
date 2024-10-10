2ab5a0751ffc7951c90a4d17e7547f673e7ee377
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-10-10 15:09:40 +0900

    필터 문제 어떻게 해보면 좋을까

--- Client/Private/Effect.cpp
+++ Client/Private/Effect.cpp
@@ -27,6 +27,9 @@ HRESULT CEffect::Initialize(void* pArg)
 	if (FAILED(__super::Initialize(pArg)))
 		return E_FAIL;
 
+	if (FAILED(Ready_Components()))
+		return E_FAIL;
+
 	return S_OK;
 }
 
@@ -37,8 +40,7 @@ void CEffect::Priority_Update(_float fTimeDelta)
 
 void CEffect::Update(_float fTimeDelta)
 {
-	for (auto& iter : m_vecColliderCom)
-		iter->Update(m_pTransformCom->Get_WorldMatrix());
+	m_pColliderCom->Update(m_pTransformCom->Get_WorldMatrix());
 
 	if (GetAsyncKeyState(VK_UP) & 0x8000)
 		m_fY -= 0.1;
@@ -53,18 +55,14 @@ void CEffect::Update(_float fTimeDelta)
 		m_fX += 0.1;
 
 	_float2 DefaultFloat2 = _float2(-5.f, 5.f);
-	Make_Effect_Collider_EnergyAttack(CCollider_Manager::CG_1P_SKILL, DefaultFloat2, _float2(DefaultFloat2.x + m_fX, DefaultFloat2.y + m_fY));
+	Make_Ray(CCollider_Manager::CG_1P_SKILL, DefaultFloat2, _float2(DefaultFloat2.x + m_fX, DefaultFloat2.y + m_fY));
 }
 
 void CEffect::Late_Update(_float fTimeDelta)
 {
 	m_pRenderInstance->Add_RenderObject(CRenderer::RG_NONBLEND, this);
 
-	for (auto& iter : m_vecColliderCom)
-#ifdef _DEBUG
-		m_pRenderInstance->Add_DebugComponent(iter);
-#endif
-
+	m_pRenderInstance->Add_DebugComponent(m_pColliderCom);
 }
 
 HRESULT CEffect::Render(_float fTimeDelta)
@@ -87,7 +85,7 @@ void CEffect::OnCollisionExit(CCollider* other)
 	int a = 3;
 }
 
-void CEffect::Make_Effect_Collider_EnergyAttack(CCollider_Manager::COLLIDERGROUP eColliderGroup, _float2 SourcePos, _float2 DestPos)
+void CEffect::Make_Ray(CCollider_Manager::COLLIDERGROUP eColliderGroup, _float2 SourcePos, _float2 DestPos)
 {
 	// 1. 시작점과 끝점 사이의 거리 및 방향 계산
 	_float dx = DestPos.x - SourcePos.x;
@@ -96,65 +94,25 @@ void CEffect::Make_Effect_Collider_EnergyAttack(CCollider_Manager::COLLIDERGROUP
 	// 두 점 사이의 거리 계산
 	_float distance = sqrtf(dx * dx + dy * dy);
 
-	// 에네르기파가 아직 생성되지 않았을 때 처리
-	if (distance < m_UnitSize.x)
-		return;
-
 	// 방향 벡터 및 정규화
 	_float2 direction = { dx / distance, dy / distance };
+}
 
-	// 2. 필요한 콜라이더의 개수 계산
-	_float unitLength = m_UnitSize.x; // 단위 콜라이더의 가로 크기 (예:  3.f)
-	int requiredColliders = static_cast<int>(ceil(distance / unitLength));
+HRESULT CEffect::Ready_Components()
+{
+	CBounding_AABB::BOUNDING_AABB_DESC	BoundingDesc{};
 
-	// 3. 현재 콜라이더 그룹의 콜라이더 수 확인
-	int currentColliders = static_cast<int>(m_vecColliderCom.size());
+	BoundingDesc.vExtents = _float3(1.5f, 1.5f, 1.5f);
+	BoundingDesc.vCenter = _float3(0.f, 0.f, 0.f);
+	BoundingDesc.pMineGameObject = this;
 
-	// 4. 필요한 경우 콜라이더 추가 생성
-	for (int i = currentColliders; i < requiredColliders; ++i)
-	{
-		// 콜라이더 추가 생성
-		CBounding_AABB::BOUNDING_AABB_DESC BoundingDesc{};
+	if (FAILED(__super::Add_Component(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Collider_AABB"),
+		TEXT("Com_Collider"), reinterpret_cast<CComponent**>(&m_pColliderCom), &BoundingDesc)))
+		return E_FAIL;
 
-		BoundingDesc.vExtents = _float3(m_UnitSize.x / 2.0f, m_UnitSize.y / 2.0f, 0.5f); // Extents는 반지름 값이므로 /2.0f
-		BoundingDesc.vCenter = _float3(SourcePos.x, SourcePos.y, 0.f); // 초기 위치를 SourcePos로 설정
-		BoundingDesc.pMineGameObject = this;
+	m_pGameInstance->Add_ColliderObject(CCollider_Manager::CG_1P_SKILL, m_pColliderCom);
 
-		CCollider* pNewCollider = nullptr;
-
-		// 고유한 컴포넌트 이름 생성 (예: Com_Collider_0, Com_Collider_1, ...)
-		_wstring colliderName = L"Com_Collider_" + to_wstring(i);
-
-		if (FAILED(__super::Add_Component(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Collider_AABB"),
-			colliderName.c_str(), reinterpret_cast<CComponent**>(&pNewCollider), &BoundingDesc)))
-			return; // 에러 처리
-
-		// 콜라이더 벡터에 추가
-		m_vecColliderCom.push_back(pNewCollider);
-	}
-
-	// 5. 콜라이더 위치 업데이트
-	// 각 콜라이더의 위치 설정
-	for (int i = 0; i < requiredColliders; ++i)
-	{
-		// 콜라이더의 중점 위치 계산
-		_float currentDistance = unitLength * (i + 0.5f);
-		_float2 colliderPos = {
-			SourcePos.x + direction.x * currentDistance,
-			SourcePos.y + direction.y * currentDistance
-		};
-
-		// 콜라이더 가져오기
-		CCollider* collider = m_vecColliderCom[i];
-
-		// 현재 콜라이더의 원래 위치와 크기를 가져오기
-		BoundingBox boundingBox = collider->AABB_GetDesc();
-		boundingBox.Center = _float3(colliderPos.x, colliderPos.y, 0.f);
-		collider->AABB_SetDesc(boundingBox);
-
-		// 콜라이더 매니저에 추가
-		m_pGameInstance->Add_ColliderObject(eColliderGroup, collider);
-	}
+	return S_OK;
 }
 
 CEffect* CEffect::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
@@ -185,8 +143,7 @@ CGameObject* CEffect::Clone(void* pArg)
 
 void CEffect::Free()
 {
-	for (auto& iter : m_vecColliderCom)
-		Safe_Release(iter);
+	Safe_Release(m_ColliderCom);
 
 	__super::Free();
 
--- Client/Public/Effect.h
+++ Client/Public/Effect.h
@@ -30,16 +30,16 @@ public:
 	virtual void OnCollisionStay(class CCollider* other, _float fTimeDelta) override;
 	virtual void OnCollisionExit(class CCollider* other) override;
 
-	void Make_Effect_Collider_EnergyAttack(CCollider_Manager::COLLIDERGROUP eColliderGroup, _float2 SourcePos, _float2 DestPos);
+	void Make_Ray_Collider(CCollider_Manager::COLLIDERGROUP eColliderGroup, _float2 SourcePos, _float2 DestPos);
 
+	HRESULT Ready_Components();
 
 private:
-	vector<CCollider*>				m_vecColliderCom;
+	CCollider*				m_pColliderCom;
 
 	_float					m_fRandom = {};
 	_float					m_fX = 0.f;
 	_float					m_fY = 0.f;
-	_float2					m_UnitSize = { 3.f,3.f };
 
 public:
 	static CEffect* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
