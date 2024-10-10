ffee36415acf296ebc6270dbf8a401318d61431b
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-10-10 19:52:45 +0900

    깃 이그노어 수정, 쉐이더 경로 수정

---
+++ EngineSDK/Inc/Engine_Struct.h
@@ -0,0 +1,140 @@
+#pragma once
+#include "Engine_Typedef.h"
+
+
+namespace Engine
+{
+	typedef struct
+	{
+		XMFLOAT3		vScale;
+		XMFLOAT4		vRotation;
+		XMFLOAT3		vPosition;
+
+		/* KEYFRAME : 뼈의 상태 */
+		/* fTime : 이 뼈가 이 fTime일때 KeyFrame의 상태를 취한다. */
+		float			fTime;
+	}KEYFRAME;
+
+	typedef struct
+	{
+		XMFLOAT3		vScale;
+		XMFLOAT4		vRotation;
+		XMFLOAT3		vPosition;
+
+		float			fCurTime;
+		float			fDuration;
+	}EFFECT_KEYFRAME;
+
+	typedef struct
+	{
+		class CTexture* pMaterials[AI_TEXTURE_TYPE_MAX];
+	}MESHMATERIAL;
+
+	struct ENGINE_DLL LIGHT_DESC
+	{
+		enum TYPE { TYPE_DIRECTIONAL, TYPE_POINT, TYPE_END };
+
+		TYPE			eType;
+		XMFLOAT4		vDirection;
+		XMFLOAT4		vPosition; /* In.WorldSpace*/
+		float			fRange;
+
+		/* 난반사를 정의하기위한 빛의 색상. */
+		XMFLOAT4		vDiffuse;
+		XMFLOAT4		vAmbient;
+		XMFLOAT4		vSpecular;
+	};
+
+	/*D3DDECLUSAGE*/
+	struct ENGINE_DLL VTXPOSTEX
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT2		vTexcoord;
+
+		static const unsigned int					iNumElements = { 2 };
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+	};
+
+	typedef struct
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT3		vTexcoord;
+	}VTXCUBE;
+
+	struct ENGINE_DLL VTXMESH
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT3		vNormal;
+		XMFLOAT2		vTexcoord;
+		XMFLOAT3		vTangent;
+
+		static const unsigned int					iNumElements = { 4 };
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+
+	};
+
+	struct ENGINE_DLL VTXANIMMESH
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT3		vNormal;
+		XMFLOAT2		vTexcoord;
+		XMFLOAT3		vTangent;
+		XMUINT4			vBlendIndex;
+		XMFLOAT4		vBlendWeight;
+
+		static const unsigned int					iNumElements = { 6 };
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+	};
+
+	struct BoneWeight {
+		_uint vertexID;
+		_float weight;
+	};
+
+	struct BoneData {
+		string name;
+		XMFLOAT4X4 transformationMatrix;
+		vector<BoneData> children; // Add children to store hierarchical data
+	};
+
+	struct VertexWeight {
+		_uint vertexID;
+		_float weight;
+	};
+
+	struct MeshData {
+		string name;
+		_uint vertexCount;
+		_uint indexCount;
+		vector<VTXMESH> vertices;
+		vector<VTXANIMMESH> animVertices;
+		vector<_uint> indices;
+		_uint materialIndex;
+		vector<BoneData> bones; // 뼈 데이터를 저장할 벡터 추가
+		vector<XMFLOAT4X4> offsetMatrices; // 오프셋 행렬을 저장할 벡터 추가
+	};
+
+	struct ModelHeader {
+		_bool isAnim;
+		_uint numMeshes;
+		_uint numMaterials;
+		_uint numAnimations;
+	};
+
+	struct MaterialData {
+		string texturePaths[AI_TEXTURE_TYPE_MAX];
+	};
+
+	struct ChannelData {
+		string name;
+		vector<KEYFRAME> keyframes;
+	};
+
+
+	struct AnimationData {
+		string name;
+		float duration;
+		float ticksPerSecond;
+		vector<ChannelData> channels;
+	};
+}
