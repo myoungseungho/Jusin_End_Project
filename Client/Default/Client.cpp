fbe6ae351f2dfd4aa4b190d9db01b4b5a62a3b4a
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-10-12 09:55:03 +0900

    윈도우 피킹 고침, esc로 종료 코드 클라이언트에 넣음

--- Client/Default/Client.cpp
+++ Client/Default/Client.cpp
@@ -173,8 +173,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    RECT rcWindowed = { 0, 0, g_iWinSizeX, g_iWinSizeY };
 
 
-   AdjustWindowRect(&rcWindowed, WS_OVERLAPPEDWINDOW, FALSE);
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   AdjustWindowRect(&rcWindowed, WS_POPUP, FALSE);
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_POPUP,
        CW_USEDEFAULT, 0, rcWindowed.right - rcWindowed.left, rcWindowed.bottom - rcWindowed.top,
        nullptr, nullptr, hInstance, nullptr);
 
@@ -226,34 +226,43 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         DragFinish(hDrop);
     }
     break;
+
+    case WM_KEYDOWN:
+        if (wParam == VK_ESCAPE) // ESC 키를 누르면 창 닫기
+        {
+            PostQuitMessage(0);
+        }
+        break;
+
     case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
         {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다.
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(g_hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
+        case IDM_ABOUT:
+            DialogBox(g_hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
         }
-        break;
+    }
+    break;
+
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
-            EndPaint(hWnd, &ps);
-        }
-        break;
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
