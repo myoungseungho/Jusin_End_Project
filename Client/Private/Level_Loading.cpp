75c37781909194df4d0bf99dda8629a8545baa21
JinYoung <tick5513@naver.com>
2024-10-23 10:33:31 +0900

    누수 상태

--- Client/Private/Level_Loading.cpp
+++ Client/Private/Level_Loading.cpp
@@ -6,7 +6,7 @@
 #include "Level_GamePlay.h"
 
 #include "GameInstance.h"
-
+#include "SpaceSun.h"
 #include "UIObject.h"
 
 #include "UI_Loading_BG.h"
@@ -45,37 +45,55 @@ HRESULT CLevel_Loading::Ready_Prototype_Component()
 	if (m_bIsLevelPrepared)
 		return S_OK;
 
-	/* For.Prototype_Component_Texture_UI_LoadingBackGround */
-	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingBackGround"),
-		CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/CmnBG/tex/E3_Title_BG01.png")))))
-		return E_FAIL;
-
-	/* For.Prototype_GameObject_UI_Loading */
-	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_Loading"),
-		CUI_Loading_BG::Create(m_pDevice, m_pContext))))
-		return E_FAIL;
-
-	/* For.Prototype_Component_Texture_UI_GameStartCircle */
-	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingMark"),
-		CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/3.InGame/Middle/GameStart/GameStart%d.png"), 8))))
-		return E_FAIL;
-
-
-	/* For.Prototype_GameObject_UI_LoadingMark */
-	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_LoadingMark"),
-		CUI_LoadingMark::Create(m_pDevice, m_pContext))))
-		return E_FAIL;
-
-	/* For.Prototype_Component_Texture_UI_LoadingFont */
-	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingFont"),
-		CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/3.InGame/DB_load_00.png")))))
-		return E_FAIL;
-
-
-	/* For.Prototype_GameObject_UI_LoadingFont */
-	if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_LoadingFont"),
-		CUI_Loading_Font::Create(m_pDevice, m_pContext))))
-		return E_FAIL;
+	//_matrix			PreTransformMatrix = XMMatrixIdentity();
+	//
+	//PreTransformMatrix = XMMatrixScaling(0.01f, 0.01f, 0.01f);
+	//
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Model_SpaceSun"),
+	//	CModel::Create(m_pDevice, m_pContext, "../Bin/Resources/Map/Space/Sun/Sun.bin", PreTransformMatrix))))
+	//	return E_FAIL;
+	//
+	//
+	///* For.Prototype_Component_Texture_UI_LoadingBackGround */
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingBackGround"),
+	//	CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/CmnBG/tex/E3_Title_BG01.png")))))
+	//	return E_FAIL;
+	//
+	///* For.Prototype_Component_Texture_UI_LoadingBackGround_Mask */
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingBackGround_Mask"),
+	//	CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/CharaSelect_S3/tex/stage/stage_bg_0.png")))))
+	//	return E_FAIL;
+	//
+	///* For.Prototype_GameObject_UI_Loading */
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_Loading"),
+	//	CUI_Loading_BG::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+	//
+	///* For.Prototype_Component_Texture_UI_GameStartCircle */
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingMark"),
+	//	CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/3.InGame/Middle/GameStart/GameStart%d.png"), 8))))
+	//	return E_FAIL;
+	//
+	//
+	///* For.Prototype_GameObject_UI_LoadingMark */
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_LoadingMark"),
+	//	CUI_LoadingMark::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+	//
+	///* For.Prototype_Component_Texture_UI_LoadingFont */
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Texture_UI_LoadingFont"),
+	//	CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/UI/3.InGame/DB_load_00.png")))))
+	//	return E_FAIL;
+	//
+	//
+	///* For.Prototype_GameObject_UI_LoadingFont */
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_UI_LoadingFont"),
+	//	CUI_Loading_Font::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+	//
+	//if (FAILED(m_pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_SpaceSun"),
+	//	CSpaceSun::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
 
 #pragma endregion
 
@@ -86,18 +104,21 @@ HRESULT CLevel_Loading::Ready_Prototype_Component()
 
 HRESULT CLevel_Loading::Ready_Layer()
 {
-	m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_Loading"), TEXT("Layer_UI_LoadingBackGround"));
+	//m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_Loading"), TEXT("Layer_UI_LoadingBackGround"));
+	//
+	//CUIObject::UI_DESC Desc = {};
+	//Desc.fSpeedPerSec = 50.f;
+	//Desc.fRotationPerSec = XMConvertToRadians(90.f);
+	//for (int i = 0; i < 8; i++)
+	//{
+	//	Desc.iNumUI = i;
+	//	m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_LoadingMark"), TEXT("Layer_UI_LoadingBackGround"), &Desc);
+	//}
+	//
+	//m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_LoadingFont"), TEXT("Layer_UI_LoadingBackGround"));
 
-	CUIObject::UI_DESC Desc = {};
-	Desc.fSpeedPerSec = 50.f;
-	Desc.fRotationPerSec = XMConvertToRadians(90.f);
-	for (int i = 0; i < 8; i++)
-	{
-		Desc.iNumUI = i;
-		m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_LoadingMark"), TEXT("Layer_UI_LoadingBackGround"), &Desc);
-	}
+	//m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_SpaceSun"), TEXT("Layer_UI_LoadingBackGround"));
 
-	m_pGameInstance->Add_GameObject_ToLayer(LEVEL_LOADING, TEXT("Prototype_GameObject_UI_LoadingFont"), TEXT("Layer_UI_LoadingBackGround"));
 
 	return S_OK;
 }
