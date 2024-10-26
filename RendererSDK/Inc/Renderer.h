f58208ece64ca9ec0b6aa37449f108ea8ef4e327
kimtaewan25 <kimtaewan25@gmail.com>
2024-10-26 13:45:39 +0900

    테스트 확인

--- RendererSDK/Inc/Renderer.h
+++ RendererSDK/Inc/Renderer.h
@@ -52,7 +52,7 @@ private:
 
 	list<class CGameObject*>			m_RenderObjects[RG_END];
 	vector<string>				m_PlayerStrNames;
-	vector<_float4>				m_GlowColors;
+	vector<GLOW_DESC>				m_GlowDescs;
 	list<class CComponent*>		m_DebugComponent;
 
 	class CRenderInstance* m_pRenderInstance = { nullptr };
