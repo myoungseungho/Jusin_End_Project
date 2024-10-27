2f1d7dafd22f6042ff9e2ca81eef2f4394cb189a
kimtaewan25 <kimtaewan25@gmail.com>
2024-10-27 17:37:29 +0900

    릭확인

--- Client/Private/UI_Logo_BG.cpp
+++ Client/Private/UI_Logo_BG.cpp
@@ -43,14 +43,14 @@ HRESULT CUI_Logo_BG::Initialize(void* pArg)
 	XMStoreFloat4x4(&m_ViewMatrix, XMMatrixIdentity());
 	XMStoreFloat4x4(&m_ProjMatrix, XMMatrixOrthographicLH(g_iWinSizeX, g_iWinSizeY, 0.f, 1.f));
 
-	m_hVideo = MCIWndCreate(g_hWnd,			// 부모의 윈도우 핸들을 전달
-		nullptr,		// MCI 윈도우를 사용하는 인스턴스 핸들
-		WS_CHILD | WS_VISIBLE | MCIWNDF_NOPLAYBAR, // 자식창 | 즉시 출력 | 플레이바 생성하지 않음
-		L"../Bin/Resources/Video/Video_Logo.wmv");
-	
-	MoveWindow(m_hVideo, 0, 0, g_iWinSizeX, g_iWinSizeY, FALSE);
-	
-	MCIWndPlay(m_hVideo);
+	//m_hVideo = MCIWndCreate(g_hWnd,			// 부모의 윈도우 핸들을 전달
+	//	nullptr,		// MCI 윈도우를 사용하는 인스턴스 핸들
+	//	WS_CHILD | WS_VISIBLE | MCIWNDF_NOPLAYBAR, // 자식창 | 즉시 출력 | 플레이바 생성하지 않음
+	//	L"../Bin/Resources/Video/Video_Logo.wmv");
+	//
+	//MoveWindow(m_hVideo, 0, 0, g_iWinSizeX, g_iWinSizeY, FALSE);
+	//
+	//MCIWndPlay(m_hVideo);
 
 	return S_OK;
 }
@@ -81,8 +81,6 @@ HRESULT CUI_Logo_BG::Render(_float fTimeDelta)
 
 	if (FAILED(m_pVIBufferCom->Render()))
 		return E_FAIL;
-
-	
 
 	return S_OK;
 }
@@ -159,5 +157,5 @@ void CUI_Logo_BG::Free()
 	Safe_Release(m_pShaderCom);
 	Safe_Release(m_pVIBufferCom);
 
-	MCIWndClose(m_hVideo);
+	//MCIWndClose(m_hVideo);
 }
