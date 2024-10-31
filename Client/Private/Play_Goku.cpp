9b16ed477c82e95c5a882dd6f43f63ce0014d742
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-10-31 17:40:49 +0900

    최종빌드

--- Client/Private/Play_Goku.cpp
+++ Client/Private/Play_Goku.cpp
@@ -211,16 +211,6 @@ HRESULT CPlay_Goku::Initialize(void* pArg)
 	else
 		m_bPlaying = true;
 
-
-	if (::AllocConsole() == TRUE)
-	{
-		FILE* nfp[3];
-		freopen_s(nfp + 0, "CONOUT$", "rb", stdin);
-		freopen_s(nfp + 1, "CONOUT$", "wb", stdout);
-		freopen_s(nfp + 2, "CONOUT$", "wb", stderr);
-		std::ios::sync_with_stdio();
-	}
-
 	return S_OK;
 }
 
