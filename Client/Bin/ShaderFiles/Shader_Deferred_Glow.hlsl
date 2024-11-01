e31b00fea7c3684de3dad986724a2d046358b938
kimtaewan25 <kimtaewan25@gmail.com>
2024-11-01 14:26:15 +0900

    NonLight 이펙트 아웃라인 추가 및 이펙트 글로우 강도 수정 및 플레이어 얼굴및 머리 빛 이펙트빛 연산 안받게 마스크

--- Client/Bin/ShaderFiles/Shader_Deferred_Glow.hlsl
+++ Client/Bin/ShaderFiles/Shader_Deferred_Glow.hlsl
@@ -309,7 +309,7 @@ PS_OUT PS_MAIN_RESULT_ALLEFFECT(PS_IN In)
     vector vBlur = g_BlurTexture.Sample(DestroySampler, In.vTexcoord);
    /*vector      vEffect = g_EffectTexture.Sample(LinearSampler, In.vTexcoord);*/
      
-    Out.vColor = saturate(vResult + vBlur * 4.2f) /*+ vEffect*/;
+    Out.vColor = saturate(vResult + vBlur * 3.2f) /*+ vEffect*/;
     //Out.vColor.a = saturate(Out.vColor.a - 0.3f);
     return Out;
 
