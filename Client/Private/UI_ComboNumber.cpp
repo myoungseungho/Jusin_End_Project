735f6306d41249746584feaba1d2de6134004ca1
JinYoung <tick5513@naver.com>
2024-11-01 15:39:43 +0900

    콤보 커지는 애니메이션 넣음

--- Client/Private/UI_ComboNumber.cpp
+++ Client/Private/UI_ComboNumber.cpp
@@ -177,18 +177,24 @@ void CUI_ComboNumber::ScaleAnimation()
 {
 	_bool bHit = FALSE;
 
-	if(m_pMainPawn != nullptr)
-		m_bScaleAnim = m_pMainPawn->Get_PawnDesc().bHit;
+	//if(m_pMainPawn != nullptr)
+		//m_bScaleAnim = m_pMainPawn->Get_PawnDesc().bHit;
 
+	if (m_iPrevCombo < m_iComboCount)
+		m_bScaleAnim = TRUE;
 
 	if (m_bScaleAnim)
 	{
 		__super::Set_UI_Setting(m_fSizeX * 1.25f, m_fSizeY * 1.25f, m_fPosX, m_fPosY, m_fDepth);
+		m_bScaleAnim = FALSE;
 	}
 	else 
 	{
 		__super::Set_UI_Setting(m_fSizeX, m_fSizeY, m_fPosX, m_fPosY, m_fDepth);
 	}
+
+	m_iPrevCombo = m_iComboCount;
+
 
 }
 
