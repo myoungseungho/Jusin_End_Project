24ff33f2cad6757cb61e2292cbae616475f3c274
kimtaewan25 <kimtaewan25@gmail.com>
2024-11-12 17:02:45 +0900

    aa

--- Client/Private/Effect.cpp
+++ Client/Private/Effect.cpp
@@ -100,7 +100,6 @@ EFFECT_KEYFRAME CEffect::Get_KeyFrame(_uint KeyFrameNumber)
 	Set_Effect_Position(KeyFrame.vPosition);
 	Set_Effect_Rotation(KeyFrame.vRotation);
 
-	m_pTransformCom->Set_Matrix(m_LayerMatrix);
 
 	EFFECT_KEYFRAME ResultKeyFrame;
 
@@ -118,7 +117,25 @@ EFFECT_KEYFRAME CEffect::Get_KeyFrame(_uint KeyFrameNumber)
 	ResultKeyFrame.fCurTime = KeyFrame.fCurTime;
 	ResultKeyFrame.fDuration = KeyFrame.fDuration;
 
+	m_pTransformCom->Set_Matrix(m_LayerMatrix);
 	return ResultKeyFrame;
+}
+
+void CEffect::Set_ParentMatrixMultiply_LocalMatrix()
+{
+	_float4x4 LayerMatrix;
+	_matrix WorldMatrix = m_pTransformCom->Get_WorldMatrix();
+
+	//LayerMatrix * 
+	_matrix TestMatrix = WorldMatrix * m_LayerMatrix;
+	XMStoreFloat4x4(&LayerMatrix, TestMatrix);
+	//m_pTransformCom->Set_WorldMatrix(LayerMatrix);
+
+	XMStoreFloat4x4(&m_WorldMatrix, WorldMatrix* m_LayerMatrix);
+	//m_pTransformCom->Set_Matrix(TestMatrix);
+//	m_pTransformCom->Set_Matrix(m_pTransformCom->Get_WorldMatrix_Inverse());
+	//m_pTransformCom->Set_Matrix(m_LayerMatrix);
+
 }
 
 EFFECT_KEYFRAME CEffect::Get_Near_Front_KeyFrame(_uint frameNumber)
@@ -189,8 +206,15 @@ HRESULT CEffect::Play_Animation(_float CurrentFrame)
 	Set_Effect_Position(ResultKeyFrame.vPosition);
 	Set_Effect_Rotation(ResultKeyFrame.vRotation);
 
-	m_pTransformCom->Set_Matrix(m_LayerMatrix);
-	//LookCamObject();
+	_float4x4 LayerMatrix;
+	_matrix WorldMatrix = m_pTransformCom->Get_WorldMatrix();
+
+	//LayerMatrix * 
+	_matrix TestMatrix = WorldMatrix * m_LayerMatrix;
+	XMStoreFloat4x4(&LayerMatrix, TestMatrix);
+	//m_pTransformCom->Set_WorldMatrix(LayerMatrix);
+
+	XMStoreFloat4x4(&m_WorldMatrix, WorldMatrix * m_LayerMatrix);
 
 	return S_OK;
 }
--- Client/Private/Effect_ZNone.cpp
+++ Client/Private/Effect_ZNone.cpp
@@ -225,8 +225,9 @@ HRESULT CEffect_ZNone::Ready_Components(_wstring* pModelName, _wstring* pMaskTex
 
 HRESULT CEffect_ZNone::Bind_ShaderResources()
 {
-	if (FAILED(m_pTransformCom->Bind_ShaderResource(m_pShaderCom, "g_WorldMatrix")))
+	if (FAILED(m_pShaderCom->Bind_Matrix("g_WorldMatrix", &m_WorldMatrix)))
 		return E_FAIL;
+
 
 	if (FAILED(m_pShaderCom->Bind_Matrix("g_ViewMatrix", &m_pGameInstance->Get_Transform_Float4x4(CPipeLine::D3DTS_VIEW))))
 		return E_FAIL;
