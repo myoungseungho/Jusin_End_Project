4527357a309f43c93abd5f348fcc39239ad55307
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-11-12 17:56:56 +0900

    빌보딩

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -30,11 +30,11 @@ HRESULT CLevel_GamePlay::Initialize()
 	Create_QTE_Manager();
 #pragma region ¸Ê »çº» °´Ã¼
 
-	//if (FAILED(Ready_Volcano()))
-	//	return E_FAIL;
-
-	if (FAILED(Ready_Space()))
+	if (FAILED(Ready_Volcano()))
 		return E_FAIL;
+
+	//if (FAILED(Ready_Space()))
+	//	return E_FAIL;
 #pragma endregion
 
 #pragma region Ä³¸¯ÅÍ »çº» °´Ã¼
