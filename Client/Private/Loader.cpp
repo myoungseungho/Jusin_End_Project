8882c04d4c9b20de06490e20bedbb210f9fe4235
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-11-14 16:09:59 +0900

    릭 해결

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -2492,11 +2492,10 @@ HRESULT CLoader::Load_Model_Resources_GamePlay_0()
 	//	return E_FAIL;
 
 
-
 	//È÷Æ®
-	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_untitled"),
-		CModel::Create(m_pDevice, m_pContext, "../Bin/ModelData/Hit.bin", PreTransformMatrix))))
-		return E_FAIL;
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_untitled"),
+	//	CModel::Create(m_pDevice, m_pContext, "../Bin/ModelData/Hit.bin", PreTransformMatrix))))
+	//	return E_FAIL;
 
 	//21È£
 	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_Play_21"),
