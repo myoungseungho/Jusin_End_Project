f6554bf33195ec9589ab4c4d9c86ad37ef83c142
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-11-16 12:06:53 +0900

    빌보딩 고침

--- Client/Private/Effect.cpp
+++ Client/Private/Effect.cpp
@@ -211,7 +211,17 @@ HRESULT CEffect::Play_Animation(_float CurrentFrame)
 	//_float fOriginY = XMVectorGetY(m_pTransformCom->Get_WorldMatrix().r[3]);
 	
 
+	//XMStoreFloat4x4(&m_WorldMatrix, m_pTransformCom->Get_WorldMatrix() * m_LayerMatrix);
+
 	XMStoreFloat4x4(&m_WorldMatrix, m_pTransformCom->Get_WorldMatrix() * m_LayerMatrix);
+
+	if (m_bIsBillboarding)
+	{
+		m_pTransformCom->Set_WorldMatrix(m_WorldMatrix);
+		m_pTransformCom->LookAt(m_pGameInstance->Get_CamPosition_Vector());
+		XMStoreFloat4x4(&m_WorldMatrix, m_pTransformCom->Get_WorldMatrix());
+	}
+
 	//m_WorldMatrix._43 = fOriginZ;
 	//m_WorldMatrix._42 = fOriginY;
 	
