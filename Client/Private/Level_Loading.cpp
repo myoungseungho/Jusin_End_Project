550aff755d6a866cfdaa94a19506a5c992075d1b
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-11-17 15:12:00 +0900

    진영 머지

--- Client/Private/Level_Loading.cpp
+++ Client/Private/Level_Loading.cpp
@@ -209,7 +209,8 @@ void CLevel_Loading::Update(_float fTimeDelta)
 			break;
 	
 		case LEVEL_GAMEPLAY:
-			if(m_bNextLevel)
+			//UIObject 가 false 일때 (로딩 더 줄이기 위해) 바로 게임플레이 넘어가게 끔 하는 코드
+			if(m_bNextLevel || CUI_Manager::Get_Instance()->m_bActive == FALSE)
 				pNextLevel = CLevel_GamePlay::Create(m_pDevice, m_pContext);
 	
 			break;
@@ -231,7 +232,8 @@ void CLevel_Loading::Update(_float fTimeDelta)
 				return;
 		}
 
-		else if(m_eNextLevelID != LEVEL_GAMEPLAY)
+		//UIObject 가 false 일때 (로딩 더 줄이기 위해) 바로 게임플레이 넘어가게 끔 하는 코드
+		else if(m_eNextLevelID != LEVEL_GAMEPLAY || CUI_Manager::Get_Instance()->m_bActive == FALSE)
 		{
 			if (FAILED(m_pGameInstance->Change_Level(pNextLevel)))
 				return;
