05db48c4dd469212334bffd08a198ce2f676c8cc
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-11-20 18:46:42 +0900

    히트 찍는 중

--- Client/Private/Effect_Layer.cpp
+++ Client/Private/Effect_Layer.cpp
@@ -204,7 +204,11 @@ void CEffect_Layer::Update(_float fTimeDelta)
 
 				XMMatrixDecompose(&Scale, &Rotation, &Position, XMLoadFloat4x4(m_pPlayerMatrix));
 
-				FinalMatrix._43 = 0;
+				if (pEffect->m_EffectName.find(L"Parrying_Ball") != wstring::npos)
+					FinalMatrix._43 = XMVectorGetZ(Position);
+				else
+					FinalMatrix._43 = 0;
+
 				FinalMatrix._41 = FinalMatrix._41 + XMVectorGetX(Position) /*+ m_fChangePosition.x*/;
 				FinalMatrix._42 = FinalMatrix._42 + XMVectorGetY(Position) /*+ m_fChangePosition.y*/;
 
@@ -257,8 +261,6 @@ void CEffect_Layer::Update(_float fTimeDelta)
 
 
 	m_pColliderCom->Update(m_pTransformCom->Get_State(CTransform::STATE_POSITION));
-
-
 
 }
 
