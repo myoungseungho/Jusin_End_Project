c82e7890b2f6db2f98578b4f78f0c3056055905d
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-11-22 13:42:39 +0900

    태욱이랑도 머지 완료

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -63,7 +63,7 @@ HRESULT CLevel_GamePlay::Initialize()
 	CharacterDesc.iTeam = 2;
 	CharacterDesc.ePlayerSlot = CUI_Define::RPLAYER1;
 
-	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Play_Hit"), TEXT("Layer_Character"), &CharacterDesc)))
+	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Play_Goku"), TEXT("Layer_Character"), &CharacterDesc)))
 		return E_FAIL;
 
 
@@ -142,7 +142,7 @@ HRESULT CLevel_GamePlay::Initialize()
 
 
 
-	CBattleInterface_Manager::Get_Instance()->Character_Opening_AIO();
+	//CBattleInterface_Manager::Get_Instance()->Character_Opening_AIO();
 
 	return S_OK;
 }
