2177e91344e7ba9cb5f44caa045fbb520458e9e8
LAPTOP-39J18DB3\user <intomydoor@gmail.com>
2024-11-24 16:34:48 +0900

    프리저 찍는 중

--- Client/Private/Character.cpp
+++ Client/Private/Character.cpp
@@ -1832,7 +1832,7 @@ void CCharacter::MoveKey1Team(_float fTimeDelta)
 		//m_pEffect_Manager->Copy_Layer(TEXT("Smoke02_Small"), &tDesc);
 		//m_pEffect_Manager->Copy_Layer(TEXT("Smoke04"), &tDesc);
 
-		m_pEffect_Manager->Copy_Layer(TEXT("FZ_SAJ-02"), &tDesc);
+		m_pEffect_Manager->Copy_Layer(TEXT("FZ_WJ"), &tDesc);
 
 		m_pTransformCom->Add_Move({ 0,0.3f,0 });
 
