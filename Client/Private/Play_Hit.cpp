00ae11273ace741ade757ea49d8ca90aa76ab841
kimtaewan25 <kimtaewan25@gmail.com>
2024-11-25 10:54:54 +0900

    a

--- Client/Private/Play_Hit.cpp
+++ Client/Private/Play_Hit.cpp
@@ -4013,8 +4013,8 @@ void CPlay_Hit::AttackEvent(_int iAttackEvent, _int AddEvent)
 				Desc.bCameraZoom = false;
 
 
-				Desc.fCameraShakeDuration = 0.1f;
-				Desc.fCameraShakeMagnitude = 0.1f;
+				Desc.fCameraShakeDuration = 0.05f;
+				Desc.fCameraShakeMagnitude = 0.03f;
 
 
 				m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Attack_Energy"), TEXT("Layer_AttackObject"), &Desc);
