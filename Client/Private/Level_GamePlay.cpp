55632fd1fd079fdc2c8bf5a1baef4202bc2159a9
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-11-28 20:26:09 +0900

    21 같이 나오는것도 추가

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -168,8 +168,8 @@ HRESULT CLevel_GamePlay::Initialize()
 
 	CBattleInterface_Manager::Get_Instance()->Character_Opening_AIO();
 
-	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Model_Preview"), TEXT("Layer_Model_Preview"))))
-		return E_FAIL;
+	//if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Model_Preview"), TEXT("Layer_Model_Preview"))))
+	//	return E_FAIL;
 
 	return S_OK;
 }
