550bd3f7b95d32733166941b0d793c90fe88c782
DESKTOP-4ED1A3K\명승호 <myoung6458@naver.com>
2024-11-28 17:18:26 +0900

    컷신 자막 다 추가, 이제 마지막 히트 승리 애니메이션 작업만 하면될듯

--- Client/Private/SubTitle_Manager.cpp
+++ Client/Private/SubTitle_Manager.cpp
@@ -18,6 +18,10 @@ HRESULT CSubTitle_Manager::Initialize(ID3D11Device* pDevice, ID3D11DeviceContext
 	m_mapSubTitleText[GOKU_VS_FRIEZA_SUBTITLE_2] = L"요..용서 못해.. 잘도.. 잘도..";
 	m_mapSubTitleText[GOKU_VS_FRIEZA_SUBTITLE_3] = L"뭐..뭐냐..!?";
 	m_mapSubTitleText[GOKU_VS_FRIEZA_SUBTITLE_4] = L"나 화났다----!!! 프리저---!!!!!";
+	m_mapSubTitleText[HUMAN_21_SUBTITLE_0] = L"자, 즐겁고 즐거운 사냥 시간이야";
+	m_mapSubTitleText[FRIEZA_SUBTITLE_0] = L"워밍업은 이쯤 해두고 슬슬 진심으로 해볼까...";
+	m_mapSubTitleText[GOKU_SUBTITLE_0] = L"나는 초사이어인, 손오공이다";
+
 	m_pSubTitle = static_cast<CSubTitle*>(m_pGameInstance->Clone_GameObject(TEXT("Prototype_GameObject_SubTitle")));
 
 	return S_OK;
