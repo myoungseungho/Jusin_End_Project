5f1462a21f7bdb908d008a31699d6927348010f6
kimtaewan25 <kimtaewan25@gmail.com>
2024-11-28 14:19:16 +0900

    1p텍스쳐바꿈

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -1664,7 +1664,7 @@ HRESULT CLoader::Load_Texture_Resources_GamePlay_1()
 		return E_FAIL;
 	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Texture_S21_2P"),
 		CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/ModelData/TON_base_2P.png"), 1))))
-		return E_FAIL;aa
+		return E_FAIL;
 	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Texture_S21_1P"),
 		CTexture::Create(m_pDevice, m_pContext, TEXT("../Bin/ModelData/TON_base.png"), 1))))
 		return E_FAIL;
